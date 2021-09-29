f0e9b8208e0fc5e63f4d21f512117563af4dfdd6
unknown <yeasin.mohammod018@gmail.com>
2021-09-30 01:49:23 +0600

    done from course

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 // console.log('JavaScript working...')
 
-window.addEventListener()
+window.addEventListener("DOMContentLoaded", (event) => {
+    let topButton = document.getElementById('topButton');
+
+    window.onscroll = () => {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            topButton.style.display = 'block';
+        }
+        else {
+            topButton.style.display = 'none';
+        }
+    }
+
+});
